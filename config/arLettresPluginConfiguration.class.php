a780e286bf62c87d14021a49d594995ae894b235
Luca Guindani <luca.guindani@gmail.com>
2021-06-03 10:23:08 +0200

    Initial commit

---
+++ config/arLettresPluginConfiguration.class.php
@@ -0,0 +1,27 @@
+<?php
+
+   class arLettresPluginConfiguration extends sfPluginConfiguration
+   {
+     public static
+       $summary = 'Theme plugin for the UNIL Faculty of Arts, extension of arDominionPlugin.',
+       $version = '0.0.1';
+
+     public function contextLoadFactories(sfEvent $event)
+     {
+       $context = $event->getSubject();
+       $context->response->addStylesheet('/plugins/arLettresPlugin/css/min.css', 'last', array('media' => 'all'));
+     }
+
+     public function initialize()
+     {
+       $this->dispatcher->connect('context.load_factories', array($this, 'contextLoadFactories'));
+
+       $decoratorDirs = sfConfig::get('sf_decorator_dirs');
+       $decoratorDirs[] = $this->rootDir.'/templates';
+       sfConfig::set('sf_decorator_dirs', $decoratorDirs);
+
+       $moduleDirs = sfConfig::get('sf_module_dirs');
+       $moduleDirs[$this->rootDir.'/modules'] = false;
+       sfConfig::set('sf_module_dirs', $moduleDirs);
+     }
+   }
